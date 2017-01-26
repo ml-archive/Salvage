183b2e7e7173796278b6385962a3feb7c2dbf5f1
Andrew Grosner <abgrosner@gmail.com>
2017-01-26 14:10:15 -0500

    [persist] fix issue where package private fields caused filer exceptions. update version to 1.0.6.

---
+++ app/src/main/java/com/fuzz/android/salvager/objects/java/ReferenceClass.java
@@ -0,0 +1,12 @@
+package com.fuzz.android.salvager.objects.java;
+
+import com.fuzz.android.salvage.core.Persist;
+import com.fuzz.android.salvager.objects.java.packageprivate.PackagePrivateObject;
+
+/**
+ * Description:
+ */
+
+@Persist
+public class ReferenceClass extends PackagePrivateObject {
+}
